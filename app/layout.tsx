39dfb67642a9fb7a2082fe0739a0783d88358b93
mecoderforhad <forhadahmed.cse@gmail.com>
2025-01-17 20:35:47 +0600

    setup redux for state management

--- app/layout.tsx
+++ app/layout.tsx
@@ -2,6 +2,7 @@ import type { Metadata } from "next";
 import { Geist, Geist_Mono } from "next/font/google";
 import { Toaster } from "react-hot-toast";
 import { NextAuthProvider } from "./(auth)/signin/session";
+import StoreProvider from "./StoreProvider";
 import "./globals.css";
 
 const geistSans = Geist({
@@ -29,8 +30,10 @@ export default function RootLayout({
       <body
         className={`${geistSans.variable} ${geistMono.variable} antialiased`}
       >
-        <Toaster position="top-right" reverseOrder={false} />
-        <NextAuthProvider>{children}</NextAuthProvider>
+        <StoreProvider>
+          <Toaster position="top-right" reverseOrder={false} />
+          <NextAuthProvider>{children}</NextAuthProvider>
+        </StoreProvider>
       </body>
     </html>
   );
